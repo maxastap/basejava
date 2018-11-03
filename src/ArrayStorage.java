6aa537ee1c7950c5be6f70ef5a73b893fb67fe32
Astapchyck Maxim <maxastap@tut.by>
2018-11-03 16:19:22 +0300

    v last not work

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,61 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    int count = 0;
     void clear() {
+        for (int i = 0; i<=storage.length-1; i++){
+            storage[i]=null;
+        }
     }
 
     void save(Resume r) {
+
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
+        for (int i=0; i<=size();i++){
+            if (storage[i].uuid.equals(uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+
+        for(int i = 0; i<=size(); i++){
+            if(storage[i].uuid==uuid) {
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
+                count--;
+            }
+        }
+           /*
+            }*/
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume allList[] = new Resume[size()];
+        System.out.println(allList.length);
+        for(int i = 0; i<=size()-1; i++){
+            allList[i]=storage[i];
+        }
+        return allList;
     }
 
     int size() {
-        return 0;
+        int size =0;
+        for(int i=0; i<=storage.length-1;i++){
+            if(storage[i]!=null)
+                size++;
+        }
+        return size;
     }
 }
